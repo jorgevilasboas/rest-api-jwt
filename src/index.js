b00d1d33e52056c26afdb1a9e6bc5f1ac1229a1b
jorgevilasboas <jorge.sergipe@gmail.com>
2018-04-03 20:40:13 -0300

    Adds user registration

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ const bodyParser  = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+require('./controllers/authController')(app);
 
 app.listen(3000, function(){
-    console.log('Server started...');
+    console.log('Server started...');    
 })
-
-
