3da01affa230a22211d4e64cd0f0b45065fc2e38
jorgevilasboas <jorge.sergipe@gmail.com>
2018-04-03 20:50:04 -0300

    minor changes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,9 +5,16 @@ const User = require('../models/user');
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {    
+router.post('/register', async (req, res) => {   
+    const {email} = req.body;
     try {
+        if (await User.findOne({email}))
+            return res.status(400).send({error: "user already exists"});
+        
         const user =  await User.create(req.body);
+
+        user.password = undefined;
+
 
         return res.send({user});
 
