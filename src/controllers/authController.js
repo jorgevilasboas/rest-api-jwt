b8fd5ca4849fd46e6ea17e1e2756b65c7ef47187
jorgevilasboas <jorge.sergipe@gmail.com>
2018-04-04 09:25:39 -0300

    Adds Aula02 10min

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
 const User = require('../models/user');
 const router = express.Router();
 
@@ -31,11 +33,13 @@ router.post('/authenticate', async (req, res) => {
     if (!await bcrypt.compare(password, user.password))
         return res.status(400).send({error: 'invalid password'});
     
-        user.password = undefined;        
-    
-    res.send({
-        user
+    user.password = undefined;
+
+    const token = jwt.sign({id: user.id}, authConfig.secret, {
+        expiresIn: 86400,
     });
+
+    res.send({ user, token });
 });
 
 module.exports = (app) => app.use('/auth', router);
