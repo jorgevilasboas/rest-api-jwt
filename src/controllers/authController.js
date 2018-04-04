d789d2087bf2a8e6ea65f30daae591d8ecac44c7
jorgevilasboas <jorge.sergipe@gmail.com>
2018-04-04 09:03:00 -0300

    Adds Aula02 6min

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,6 @@
 const express = require('express');
-
-
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
-
 const router = express.Router();
 
 router.post('/register', async (req, res) => {   
@@ -15,12 +13,29 @@ router.post('/register', async (req, res) => {
 
         user.password = undefined;
 
-
         return res.send({user});
 
     } catch (err) {
         return res.status(400).send({error: 'registration failed'});
     }
+});
+
+router.post('/authenticate', async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email}).select('+password');
+
+    if (!user)
+        return res.status(400).send({error: 'User not found'});
+    
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).send({error: 'invalid password'});
+    
+        user.password = undefined;        
+    
+    res.send({
+        user
+    });
 });
 
 module.exports = (app) => app.use('/auth', router);
